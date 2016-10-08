9b316e9dfe2f1027d691dc2d50e99a09c24359cf
nickv97 <nick_vandyke97@yahoo.com>
2016-10-08 04:24:31 -0400

    Add files via upload

--- balderdashy2.cpp
+++ balderdashy2.cpp
@@ -3,13 +3,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cassert>
+#include <vector>
 
 using namespace std;
 //functions
 
+struct Definition {
+
+	string words;
+	int player_number;
+	bool correct;
+
+};
+
+void get_file_info(vector<string>* v, ifstream fin){
 
 
 
+}
 
 //main
 int main() {
@@ -18,10 +30,11 @@ int main() {
 
 	cout << "Welcome to Balderdashy2!\nPlease enter the number of players: ";
 	cin >> players;
+	assert(players > 0);
 
 	cout << "\nWe will play with " << players << " players.\n";
 
-	
+
 
 	return 0;
 }
