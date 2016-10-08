3ea68283476f3c65b4077747585effad28a354e1
Nathan Kurt <nate.kurt@gmail.com>
2016-10-08 07:30:48 -0400

    Added a try except and a key function for sorting.
    Just need to find a way to replace actual value into the array.

--- balder.py
+++ balder.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import random
-players = input("Welcome to Balderdashy2!\nPlease enter the number of players: ")
+while True:
+    try:
+        players = int(input("Welcome to Balderdashy2!\nPlease enter the number of players: "))
+        break
+    except (ValueError, TypeError):
+        print("Oops! that was no valid number. Try again...")
+
 
 #player_count = 0
 #if server response success add one
 
+def get_key(item): #for sorting the list
+    return item[0] #first element sorting - the number
+
+
+def random_list(item):
 
 
 print('We will play with', players, 'players.\n')
@@ -41,15 +52,18 @@ for player_num in range(players): #checks
     print("Player ", player_num, "Please enter a defintion: "))
     definition = input() #add player defintion
 
-    player_def[player_num][0].append(defintion) #I think this works add def to player list
+    player_def[player_num].append(defintion) #I think this works add def to player list
     continue
 
 
 random_order = []
 #makes sure each number is unique
-random_order = random.sample(range(1,players+2), players+1) #plus 2 because range
+random_order.append(random.sample(range(1,players+2), players+1)) #plus 2 because range
 
 for def_number in range(players+1) #players + definition added
 
-
+    player_def
     all_def[def_number][]
+
+
+#sort items now
